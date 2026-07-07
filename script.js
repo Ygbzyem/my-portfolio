103518654a06f125c270370f840a7e667d57e55b
AnhBaChaCuu <tungdepzai11032006@gmail.com>
2026-07-07 19:52:46 +0700

    fix timezone

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-function updateClock(){
+function updateClock() {
 
     const now = new Date();
 
-    const vn = new Intl.DateTimeFormat("en-US",{
-        timeZone:"Asia/Ho_Chi_Minh",
-        hour:"2-digit",
-        minute:"2-digit",
-        second:"2-digit",
-        hour12:false
+    // Hiển thị ngày
+    const date = new Intl.DateTimeFormat("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
     }).format(now);
 
-    const us = new Intl.DateTimeFormat("en-US",{
-        timeZone:"America/New_York",
-        hour:"2-digit",
-        minute:"2-digit",
-        second:"2-digit",
-        hour12:false
+    // Giờ Việt Nam
+    const vn = new Intl.DateTimeFormat("en-US", {
+        timeZone: "Asia/Ho_Chi_Minh",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false
     }).format(now);
 
-    document.getElementById("vn-time").textContent =
-        `🇻🇳 Vietnam ${vn}`;
+    // Giờ New York
+    const us = new Intl.DateTimeFormat("en-US", {
+        timeZone: "America/New_York",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false
+    }).format(now);
 
-    document.getElementById("us-time").textContent =
-        `🇺🇸 New York ${us}`;
+    document.getElementById("current-date").textContent = `📅 ${date}`;
+    document.getElementById("vn-time").textContent = `🇻🇳 Vietnam     ${vn}`;
+    document.getElementById("us-time").textContent = `🇺🇸 New York    ${us}`;
 }
 
 updateClock();
-
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
